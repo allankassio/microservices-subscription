ce632da045b5a0031838a656723afd4f65750134
Allan Cruz <allan.cruz@ifma.edu.br>
2022-07-02 19:16:53 +0100

    utils

---
+++ public-service/src/main/java/com/microservicesubscription/publicservice/utils/HttpEntityConfig.java
@@ -0,0 +1,15 @@
+package com.microservicesubscription.publicservice.utils;
+
+import com.microservicesubscription.publicservice.model.Subscription;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * @author Allan Cruz (https://github.com/allankassio)
+ * @version 1.0
+ * @date 02/07/22 12:14
+ */
+public class HttpEntityConfig {
+
+
+}
